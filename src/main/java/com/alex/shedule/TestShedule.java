c561837ca8eb3531ae8499f4ac256e27ae38bdca
Alexey <iamsimakov@gmail.com>
2015-12-02 05:37:04 +0300

    Date replace Calendar

--- src/main/java/com/alex/shedule/TestShedule.java
+++ src/main/java/com/alex/shedule/TestShedule.java
@@ -8,7 +8,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class TestShedule
 {
@@ -16,44 +17,30 @@ public class TestShedule
     {
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
-        String resut = "";// получаем данные с внешнего ресурса
-        try
-
-        {
-            URL url = new URL("http://gu-unpk.ru/schedule");
-            urlConnection = (HttpURLConnection) url.openConnection(); //открываем соединение
+        String resut = "";
+        try {
+            Calendar calendar = new GregorianCalendar();
+            long startWeek = calendar.getTimeInMillis() - (calendar.get(Calendar.DAY_OF_WEEK) - 2) * 24 * 60 * 60 * 1000L - (calendar.get(Calendar.HOUR_OF_DAY) - 3) * 60 * 60 * 1000L - calendar.get(Calendar.MINUTE) * 60 * 1000L - calendar.get(Calendar.SECOND) * 1000L;
+            URL url = new URL("http://gu-unpk.ru/schedule/3094////" + String.valueOf(startWeek) + "/printschedule");
+            urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("GET");
-            urlConnection.setRequestProperty("Host", "gu-unpk.ru");
-            urlConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0");
-            urlConnection.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-            urlConnection.setRequestProperty("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3");
-//            urlConnection.setRequestProperty("Accept-Encoding", "gzip, deflate");
-            urlConnection.setRequestProperty("Referer", "http://gu-unpk.ru/");
-            urlConnection.setRequestProperty("Cookie", "CurrentSchedule=lesson; _ga=GA1.2.2094998912.1444770053; CurrentSchedule=lesson; StudPrepAudit=1; divisionForStuds=12; kurs=5; group=3094; PHPSESSID=1l3f3cja1l76e1j8fqoajo7c41; blind-font-size=fontsize-normal; blind-colors=color1; blind-font=sans-serif; blind-spacing=spacing-small; blind-images=imagesoff; _ym_visorc_16652224=w; _gat=1");
-            urlConnection.setRequestProperty("Connection", "keep-alive");
-            urlConnection.setRequestProperty("Cache-Control", "max-age=0");
-
-            urlConnection.connect(); //подключаемся
-            InputStream inputStream = urlConnection.getInputStream();//поток ввода
-            StringBuffer buffer = new StringBuffer(); //в эту переменную будем читать
+            urlConnection.connect();
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
             reader = new BufferedReader(new InputStreamReader(inputStream));
             String line;
             while ((line = reader.readLine()) != null)
-            {  //читаем строку из потока
-                buffer.append(line); // и пока она не пустая дописываем в буфер
+            {
+                buffer.append(line);
             }
-            resut = buffer.toString(); //буфер - это объект, переводим в строку
-            System.out.println(resut); //возвращаем строку и передаем ее в onPostExecute
+            resut = buffer.toString();
+            resut = new String(resut.getBytes(), "UTF-8");
+            System.out.println(resut);
         }
 
-        catch(
-                Exception e
-                )
-
-        {
+        catch(Exception e){
             System.out.println(e.toString());
         }
-
 
     }
 }
